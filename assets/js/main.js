94a57113a439f42528b543584c9ba989094d2a39
Kovács Dorottya <kovacsdorika@gmail.com>
2020-12-02 15:31:15 +0100

    Shortening JS code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,6 @@
 'use strict'
 
-let steps = 0;
-
-//kijelző div:
 const displayDiv = document.querySelector('.calculator__display');
-//számgombok:
-const numberDivs = Array.from(document.querySelectorAll('.btn--number'));
-//összeadás gomb:
-const additionBtn = document.querySelector('.btn--addition');
-//kivonás gomb:
-const subtractionBtn = document.querySelector('.btn--subtraction');
-//szorzás gomb:
-const multiplicationBtn = document.querySelector('.btn--multiplication');
-//osztás gomb:
-const divisionBtn = document.querySelector('.btn--division');
-
 
 const calcs = {
     '+': function (a, b) { return a + b },
@@ -22,121 +8,113 @@ const calcs = {
     '/': function (a, b) { return a / b },
     '*': function (a, b) { return a * b }
 }
-
 const signs = ['+', '-', '*', '/'];
 
+const numberDivs = Array.from(document.querySelectorAll('.btn--number'));
 
-//számgombra rákattintanak, tartalmának kinyerése
+let steps = 0;
+let resultOnScreen = false;
+
 const contentExtraction = () => {  
-    numberDivs.map(item => {
-        item.addEventListener('click', () => {
-            let currentValue = displayDiv.textContent;
-            displayDiv.textContent = currentValue + item.textContent;
-            return steps += 1  
-
+    if( resultOnScreen === false ) {
+        numberDivs.map(item => {
+            item.addEventListener('click', () => {
+                displayDiv.textContent += item.textContent;
+                return steps += 1  
+            } )
         } )
-     } )
+    }
 };
 contentExtraction();
 
+const signsDivs = Array.from(document.querySelectorAll('.btn--sign'));
 
-//jelgombra rákattintanak, tartalmának kinyerése
- const operationSignAdder = (btn) => {
-    btn.addEventListener('click', () => {
-        if(steps >= 1) {
-            let currentValue = displayDiv.textContent;
-            displayDiv.textContent = `${currentValue} ${btn.dataset.value} `;
-        }
-    }) 
-}
+const operationSignAdder = () => {
+    signsDivs.map(item => {
+        item.addEventListener('click', () => {
+            if(steps >= 1) {
+                displayDiv.textContent = `${displayDiv.textContent} ${item.dataset.value} `;
+            }
+        }) 
+    })
+};
+operationSignAdder();
 
-operationSignAdder(additionBtn);
-operationSignAdder(subtractionBtn);
-operationSignAdder(multiplicationBtn);
-operationSignAdder(divisionBtn);
-
-
-//C gombbal törlés
-const deleteBtn = document.querySelector('.btn--delete');
 
 const deleteDisplayContent = () => {
-    deleteBtn.addEventListener('click', () => {
-        displayDiv.textContent = '';
-    })
-}
+    document
+        .querySelector('.btn--delete')
+        .addEventListener('click', () => {
+            displayDiv.textContent = '';
+        })
+};
 deleteDisplayContent();
 
 
-//tizedespont
-const pointBtn = document.querySelector('.btn--dot');
 const point = () => {
-    pointBtn.addEventListener('click', () => {
-        const currentValue = displayDiv.textContent;
-        return displayDiv.textContent = `${currentValue}.`
+    document
+        .querySelector('.btn--dot')
+        .addEventListener('click', () => {
+            return displayDiv.textContent = `${displayDiv.textContent}.`
     } )
 };
 point();
 
+let myNumbers = [];
+let mySigns = [];
 
-//egyenlőségjel
+const resultStrToArray = () => {
+    (displayDiv.textContent)
+        .split(' ')
+        .filter( (item, index) => 
+                index % 2 === 0 ? myNumbers.push(item) : mySigns.push(item) )
+};
+
+const errorSearch = (arr) => {
+    if(arr.some(item => item === "")) {
+        displayDiv.textContent = "ERROR";
+        return
+    }
+};
+
+const parsedNumbersArr = (arr) => {
+    return arr.map(item => parseFloat(item));
+    
+}
+
+const calculation = (arr1, arr2) => {
+    let counter = 0;
+    let resultValue;
+    for( let keys in calcs ) {
+        if(calcs.hasOwnProperty(keys)) {
+            for (let i = 0; i < arr1.length; i += 1) {
+                if(counter == 0) {
+                    resultValue = calcs[mySigns[0]](arr1[0], arr1[1]);
+                    arr1.splice(0, 2);
+                    arr2.splice(0, 1);
+                    counter += 1;
+                }
+                if(arr1.length > 0) {
+                    resultValue = calcs[mySigns[0]](resultValue, arr1[0]);
+                    arr1.splice(0, 1);
+                    arr2.splice(0, 1);
+                }
+            }
+        }
+    }
+    return displayDiv.textContent = resultValue;
+}
+
 const equalBtn = document.querySelector('.btn--sum');
-
-let resultValue;
 
 const result = () => {
     equalBtn.addEventListener('click', () => {
-        let numbers = [];
-        let signs = [];
-
-        //kiolvasom, szétvágom:
-        let resultStrToArray = (displayDiv.textContent).split(' ');
-        console.log('Szétvágott tömb: ', resultStrToArray);
-        
-        //szétszedem számokra és jelekre:
-        resultStrToArray.filter( (item, index) => 
-            index % 2 === 0 ? numbers.push(item) : signs.push(item) );
-        console.log('Numbers tömb: ', numbers);
-        console.log('Signs tömb: ', signs);
-        
-        //ERROR: ha a számok tömbben van üres string, akkor írja ki, hogy ERROR:
-        if(numbers.some(item => item === "")) {
-            displayDiv.textContent = "ERROR";
-        }
-        
-        //"számokat" átalakítom számmá:
-        let parsedArr = numbers.map(item => parseFloat(item));
-        console.log(parsedArr);
-
+        resultStrToArray();
+        errorSearch(myNumbers);
+        parsedNumbersArr(myNumbers);
         steps = 0;
-
-//kivenni: numbers 0. és 1. eleme
-//kivenni: signs 0. eleme
-//keres: calcs objektumban a signs 0. elemét -- let sign = ["*"]
-//behelyettesít: calcs[i](numbers[0], numbers[1])
-        let counter = 0;
-        for( let keys in calcs ) {
-            if(calcs.hasOwnProperty(keys)) {
-                
-                for (let i = 0; i < parsedArr.length; i += 1) {
-                    if(counter == 0) {
-                        resultValue = calcs[signs[0]](parsedArr[0], parsedArr[1]);
-                        parsedArr.splice(0, 2);
-                        console.log('Elejéről levágott parsedArr tömb: ', parsedArr);
-                        signs.splice(0, 1);
-                        console.log('Elejéről levágott Signs tömb: ', signs);
-                        counter += 1;
-                    }
-                        resultValue = calcs[signs[0]](resultValue, parsedArr[0]);
-                        parsedArr.splice(0, 1);
-                        console.log('Tovább módosított parsedArr tömb: ', parsedArr);
-                        signs.splice(0, 1);
-                        console.log('Tovább módosított Signs tömb: ', signs);
-                    
-                    }
-                    
-                }
-        }
-        return displayDiv.textContent = resultValue;
+        calculation(parsedNumbersArr(myNumbers), mySigns);
+        return resultOnScreen = true;
     } )
 }
 result();
